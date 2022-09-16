b7b58d2132e35bdf85f1ae555be01a5db1063e86
dopamine-s <motor55005@gmail.com>
2022-09-16 04:17:42 +0300

    passes data to UsersList

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -15,7 +15,7 @@ export default function AddUser(props) {
     if (+enteredAge < 1) {
       return;
     }
-    console.log(enteredUsername, enteredAge);
+    props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername('');
     setEnteredAge('');
   };
